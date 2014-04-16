a380c3b5c6290a45ad1e59330a826cf7232ea14b
Dhaval Patel <pdhaval@codeaurora.org>
2014-04-16 15:30:29 -0700

    msm: mdss: send panic/roburst signal to bimc based on fill level
    MDP hardware will send panic/roburst signal to bimc based
    on fill level for all pipes connected to realtime interferfaces.
    This allows bimc to priortize the MDP traffic across all bimc
    ports, regardless of other clients' priority. This feature
    is not for non-realtime clients like writeback.
    
    Change-Id: Iafe891c6aefad905d482bd7aa54e00562698676e
    Signed-off-by: Dhaval Patel <pdhaval@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdss/mdss_mdp.h
    	include/uapi/linux/msm_mdp.h

--- drivers/video/msm/mdss/mdss_mdp.h
+++ drivers/video/msm/mdss/mdss_mdp.h
@@ -363,6 +363,7 @@ struct mdss_mdp_pipe {
 	char __iomem *base;
 	u32 ftch_id;
 	u32 xin_id;
+	u32 panic_ctrl_ndx;
 	struct mdss_mdp_shared_reg_ctrl clk_ctrl;
 	struct mdss_mdp_shared_reg_ctrl clk_status;
 	struct mdss_mdp_shared_reg_ctrl sw_reset;
@@ -549,11 +550,20 @@ static inline int mdss_mdp_line_buffer_width(void)
 	return MAX_LINE_BUFFER_WIDTH;
 }
 
+
 static inline struct clk *mdss_mdp_get_clk(u32 clk_idx)
 {
 	if (clk_idx < MDSS_MAX_CLK)
 		return mdss_res->mdp_clk[clk_idx];
 	return NULL;
+}
+
+static inline int mdss_mdp_panic_signal_supported(
+	struct mdss_data_type *mdata, struct mdss_mdp_pipe *pipe)
+{
+	return (IS_MDSS_MAJOR_MINOR_SAME(mdata->mdp_rev, MDSS_MDP_HW_REV_105) &&
+		pipe->mixer_left &&
+		pipe->mixer_left->type == MDSS_MDP_MIXER_TYPE_INTF);
 }
 
 irqreturn_t mdss_mdp_isr(int irq, void *ptr);
@@ -729,6 +739,7 @@ int mdss_mdp_ctl_addr_setup(struct mdss_data_type *mdata, u32 *ctl_offsets,
 		u32 *wb_offsets, u32 len);
 
 int mdss_mdp_pipe_fetch_halt(struct mdss_mdp_pipe *pipe);
+int mdss_mdp_pipe_panic_signal_ctrl(struct mdss_mdp_pipe *pipe, bool enable);
 int mdss_mdp_pipe_destroy(struct mdss_mdp_pipe *pipe);
 int mdss_mdp_pipe_queue_data(struct mdss_mdp_pipe *pipe,
 			     struct mdss_mdp_data *src_data);
